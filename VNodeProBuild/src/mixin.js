8aced9aa616b5ec2c570c78aa7c19eb9f527f79a
eason <492760446@qq.com>
2018-07-26 23:38:28 +0800

    提交了设置原型连上的options

--- VNodeProBuild/src/mixin.js
+++ VNodeProBuild/src/mixin.js
@@ -5,7 +5,20 @@ export function initMixin (Eason) {
     const vm = this
     vm.uid = uid++
     debugger
-    vm.$options = options
+    if (options && options._isComponent) {
+      // 组件合并，动态属性合并缓慢，不需要特殊处理就单独拉出来优化
+      initInternalComponent(vm, options)
+    } else {
+      vm.$options = mergeOptions(
+        resolveConstructorOptions(vm.constructor),
+        options || {},
+        vm
+      )
+    }
     initState(vm)
   }
+}
+
+function resolveConstructorOptions(ctor) {
+
 }
